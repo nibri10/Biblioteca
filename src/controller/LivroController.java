9a927f5fe9c8a3000e397794e17f704830549cfe
Nicolas Oliveira <nibri.kcond2011@gmail.com>
2018-06-07 22:36:38 -0300

    Add View

--- src/controller/LivroController.java
+++ src/controller/LivroController.java
@@ -10,7 +10,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.LinkedList;
 import java.util.List;
 import javax.swing.JOptionPane;
